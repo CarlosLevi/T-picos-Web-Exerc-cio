9eba031628772621dbad2e4d1f0660213e8b9444
unknown <carloslevi2000@gmail.com>
2020-05-18 14:13:34 -0300

    Exercício Unidade 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SexoPipe } from './compartilhado/sexo.pipe';
 import { AlturaComponent } from './compartilhado/altura.component';
+import { EstudanteDetalheComponent } from './estudantes/estudante-detalhe/estudante-detalhe.component';
+import { BemVindoComponent } from './home/bem-vindo/bem-vindo.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaEstudantesComponent,
     SexoPipe,
-    AlturaComponent
+    AlturaComponent,
+    EstudanteDetalheComponent,
+    BemVindoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'estudantes', component: ListaEstudantesComponent },
+      { path: 'estudantes/:id', component: EstudanteDetalheComponent },
+      { path: 'bemvindo', component: BemVindoComponent },
+      { path: '', redirectTo: 'bemvindo', pathMatch: 'full'},
+      { path: '**', redirectTo: 'bemvindo', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
